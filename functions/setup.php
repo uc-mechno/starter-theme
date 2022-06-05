00fffed07ac22ebd2042f88bf7677a0bc5ab4043
mechno <mechnotechno@gmail.com>
2022-06-05 17:22:59 +0900

    post-thumbnails

--- functions/setup.php
+++ functions/setup.php
@@ -12,6 +12,26 @@
 function my_theme_setup()
 {
 
+// アイキャッチ画像を利用できるようにする
+add_theme_support( 'post-thumbnails' );
+
+// トップページのメイン画像用のサイズ設定
+add_image_size( 'top', 1077, 622, true );
+
+// 地域貢献活動一覧画像用のサイズ設定
+add_image_size( 'contribution', 557, 280, true );
+
+// トップページの地域貢献活動にて使用している画像用のサイズ設定
+add_image_size( 'front-contribution', 255, 189, true );
+
+// 企業情報・店舗情報一覧画像用のサイズ設定
+add_image_size( 'common', 465, 252, true );
+
+// 各ページのメイン画像用のサイズ設定
+add_image_size( 'detail', 1100, 330, true );
+
+// 検索一覧画像用のサイズ設定
+add_image_size( 'search', 168, 168, true );
 
 }
 add_action('after_setup_theme', 'my_theme_setup');
