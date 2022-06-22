c5bc95ec51a746336411a8990522f8e461280c69
mechno <mechnotechno@gmail.com>
2022-06-22 22:30:39 +0900

    検索終わり トップのみ除外

--- header.php
+++ header.php
@@ -58,16 +58,9 @@
             </ul>
 
           </nav>
-          <form class="search-form" role="search" method="get" action="">
-            <div class="search-box">
-              <input type="text" class="search-input" name="" placeholder="キーワードを入力してください" />
-              <button type="submit" class="button-submit"></button>
-            </div>
-            <div class="search-buttons">
-              <button type="button" class="close-icon js-searchIcon"></button>
-              <button type="button" class="search-icon js-searchIcon"></button>
-            </div>
-          </form>
+
+          <?php get_search_form(); ?>
+
         </div>
       </div>
     </header>
