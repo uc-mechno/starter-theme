9497c77cdcc34f3f36680ed7cf4bd64aded8e1e6
mechno <mechnotechno@gmail.com>
2022-06-26 17:03:01 +0900

    ページ送り作成#12

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -10,11 +10,5 @@
             <?php the_content(); ?>
           </div>
         </div>
-        <div class="more-news">
-          <div class="next">
-            <a class="another-link" href="#">NEXT</a>
-          </div>
-          <div class="prev">
-            <a class="another-link" href="#">PREV</a>
-          </div>
-        </div>
+
+        <?php get_template_part('template-parts/pagination', 'single'); ?>
