3efdd3d626651f8a605532cca9282ef1e8598cb4
mechno <mechnotechno@gmail.com>
2022-07-03 17:12:17 +0900

    requireに修正

--- functions/init.php
+++ functions/init.php
@@ -4,34 +4,42 @@
  *
  * init.php
  *
- * 初期設定的な関数など
+ * 初期設定
  *
  * ************************************************************************
  */
 
-/**
- * ユーザー権限毎に body にclassをふる
- * ************************************************************************
- */
-function add_user_role_class( $admin_body_class ) {
-  global $current_user;
-  if ( ! $admin_body_class ) {
-      $admin_body_class .= ' ';
-  }
-  $admin_body_class .= ' role-' . urlencode( $current_user->roles[0] );
-  return $admin_body_class;
-}
-add_filter( 'admin_body_class', 'add_user_role_class' );
+function my_theme_setup()
+{
 
-/**
- * スラッグ名のクラスを自動で出力
- * ************************************************************************
- */
-function add_class_page_slug($classes) {
-  if( is_page() ) {
-      $page = get_post( get_the_ID() );
-      $classes[] = $page->post_name;
-  }
-  return $classes;
+  /**
+   * アイキャッチ画像を利用できるようにする
+   * ************************************************************************
+   */
+  add_theme_support('post-thumbnails');
+
+  // トップページのメイン画像用のサイズ設定
+  add_image_size('top', 1077, 622, true);
+
+  // 地域貢献活動一覧画像用のサイズ設定
+  add_image_size('contribution', 557, 280, true);
+
+  // トップページの地域貢献活動にて使用している画像用のサイズ設定
+  add_image_size('front-contribution', 255, 189, true);
+
+  // 企業情報・店舗情報一覧画像用のサイズ設定
+  add_image_size('common', 465, 252, true);
+
+  // 各ページのメイン画像用のサイズ設定
+  add_image_size('detail', 1100, 330, true);
+
+  // 検索一覧画像用のサイズ設定
+  add_image_size('search', 168, 168, true);
+
+  /**
+   * 抜粋を利用できるようにする
+   * ************************************************************************
+   */
+  add_post_type_support('page', 'excerpt');
 }
-add_filter('body_class', 'add_class_page_slug');
+add_action('after_setup_theme', 'my_theme_setup');
--- functions/posts.php
+++ functions/posts.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * ************************************************************************
  *
@@ -8,3 +9,77 @@
  *
  * ************************************************************************
  */
+
+/**
+ * カスタム投稿タイプ daily_contribution を登録する
+ * ************************************************************************
+ *
+ * @link https://wpdocs.osdn.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/register_post_type
+ * @see https://prograshi.com/wordpress/wp-register_post_type/
+ * @see https://www.webdesignleaves.com/pr/wp/wp_custom_post_type.html
+ *
+ */
+function my_add_custom_post()
+{
+  register_post_type(
+    'daily_contribution', // スラッグ名
+    [
+      'label' => '地域貢献活動', // ラベル名
+      'labels' => [
+        'name' => '地域貢献活動', // ラベル名
+        'all_items' => '地域貢献活動一覧', // すべての
+        'add_new' => '新規地域貢献活動', // 新規追加
+        'edit_item' => 'レンタルの編集', // 編集
+        'new_item' => '新規レンタルの編集', // 新規
+        'view_item' => 'レンタルを表示', // 表示
+        'search_items' => 'レンタルを検索', // 検索
+        'not_found' => '見つかりません', // 見つかりません
+        'not_found_in_trash' => 'ゴミ箱内に見つかりません', // ゴミ箱内に見つかりません
+        'parent_item_colon' => '親', // 親
+      ],
+      'public' => true, // 公開
+      'has_archive' => true, // アーカイブ
+      'menu_position' => 5, // 位置
+      'show_in_rest' => true, // Gutenbergエディタを有効
+      'supports' => [
+        'title', // タイトル
+        'editor', // エディター
+        'custom-fields', // カスタムフィールド
+        'thumbnail', // アイキャッチ
+        'excerpt', // 抜粋
+        'revisions', // リビジョン
+        'page-attributes', // ページ属性
+      ],
+    ]
+  );
+}
+add_action('init', 'my_add_custom_post');
+
+/**
+ * カスタム分類 event を登録する
+ * ************************************************************************
+ */
+function my_add_taxonomy()
+{
+  register_taxonomy(
+    'event', // タクソノミー名
+    'daily_contribution', // カスタム投稿タイプ名
+    [
+      'label' => 'イベントの種類',
+      'labels' => [
+        'name' => 'イベントの種類', // ラベル名
+        'all_items' => '全てのイベント', // すべての
+        'edit_item' => 'イベントを編集', // 〇〇を編集
+        'view_item' => 'イベントを表示', // 表示
+        'update_item' => 'イベントを更新', // 〇〇を更新
+        'add_new_item' => '新規イベントを追加', // 新規追加の名前
+        'parent_item' => '親イベントを追加', // 親
+        'search_items' => 'イベントを検索', // 検索
+      ],
+      'public' => true, // 公開
+      'hierarchical' => true, // カテゴリー形式
+      'show_in_rest' => true, // Gutenberg で表示
+    ]
+  );
+}
+add_action('init', 'my_add_taxonomy');
