3f5f322d1b0fffb823a191c499a626c222b3a97e
mechno <mechnotechno@gmail.com>
2022-07-03 12:09:53 +0900

    カスタムページ追加 ディレクトリに格納#16

--- functions/widgets.php
+++ functions/widgets.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * ************************************************************************
  *
@@ -9,12 +10,20 @@
  * ************************************************************************
  */
 
- /**
+/**
  * ウィジェットの登録
  * ************************************************************************
  */
 function my_widgets_init()
 {
-
+  register_sidebar([
+    'name' => ' サイドバーウィジェットエリア',
+    'id' => 'primary-widget-area',
+    'description' => ' 固定ページのサイドバー',
+    'before_widget' => '<aside class="side-inner">',
+    'after_widget' => '</aside>',
+    'before_title' => '<h4 class="title">',
+    'after_title' => '</h4>',
+  ]);
 }
 add_action('widgets_init', 'my_widgets_init');
